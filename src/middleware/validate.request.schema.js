9ccb481ecb4f8c0f8d98848df18d0ff41b20d2a1
DarkL0rdd <vadimka1235931@gmail.com>
2022-08-18 16:52:45 +0300

    added validate request middleware

---
+++ src/middleware/validate.request.schema.js
@@ -0,0 +1,12 @@
+import { validationResult } from "express-validator";
+
+export const validateRequestSchema = (req, res, next) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array({ onlyFirstError: true }) });
+    }
+  } catch (err) {
+    next(err);
+  }
+};
